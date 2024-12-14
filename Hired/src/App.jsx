d3a258b6738c4985f65a08718476fe126d748344
Dheerajmehra003 <dheerajsinghmehra0405@gmail.com>
2024-12-14 12:48:28 +0530

    Setup-packages

---
+++ Hired/src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './Layout/AppLayout'
+import LandingPage from './Pages/LandingPage'
+import JobListing from '@/Pages/JobListing'
+import Job from '@/Pages/Job'
+import PostJob from '@/Pages/PostJob'
+import SaveJob from '@/Pages/SaveJob'
+import MyJob from '@/Pages/MyJob'
+import './App.css'
+import { ThemeProvider } from './components/ui/Theme-Provider'
+
+const router = createBrowserRouter([
+  {
+  element: <AppLayout />,
+  children: [
+        {
+          path:'/',
+          element: <LandingPage />
+        },
+        {
+          path:'/jobs',
+          element: <JobListing />
+        },
+        {
+          path:'/job/:id',
+          element: <Job />
+        },
+        {
+          path:'/post-job',
+          element: <PostJob />
+        },
+        {
+          path:'/save-job',
+          element: <SaveJob />
+        },
+        {
+          path:'/my-job',
+          element: <MyJob />
+        },
+      ]
+  }
+])
+
+
+const App = () => {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+
+  )
+}
+
+export default App
